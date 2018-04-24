3cb3de8229279cdd6199135a97fe59500d1b1a0c
David Long <david.long@racing-leagues.co.uk>
2018-04-24 14:43:55 +0100

    Initial Commit

---
+++ BullsvCows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullsvCows
+//
+//  Created by Long, David (Software Developer) on 24/04/2018.
+//  Copyright © 2018 Long, David (Software Developer). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
